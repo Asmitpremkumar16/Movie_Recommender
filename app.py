3b480462d543628cbedaa60d88874bed674280d2
Asmit Prem Kumar <asmitpremkumar@gmail.com>
2026-02-19 12:13:05 +0530

    Refactor movie recommendation logic and add download feature

--- app.py
+++ app.py
@@ -1,48 +1,73 @@
 import streamlit as st
 import pickle
+import gdown
+import os
 
 
-def recommend(movie):
-    index = movies[movies['title'] == movie].index[0]
-    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
-    recommended_movie_names = []
+movies= pickle.load(open('movies.pkl','rb'))
+file_id = "1-EHGBW_MGdvZFpQGsIcNvq4dXRVbcwxG"
+file_path = "similarity.pkl"
 
-    for i in distances[1:6]:
+# Download only if not already present
+if not os.path.exists(file_path):
+    url = f"https://drive.google.com/uc?id={file_id}"
+    gdown.download(url, file_path, quiet=False)
+
+# Load the pickle file
+similarity = pickle.load(open(file_path, "rb"))
+movies_list= movies['title'].values
+
+
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+st.header("Movie Recommender System")
+
+selected_movie= st.selectbox('Select a movie',movies_list)
+
+
+def recommend(selected_movie, n=5):
+    if selected_movie not in movies['title'].values:
+        print("Movie not found")
+        return
+
+    movie_index = movies[movies['title'] == selected_movie].index[0]
+    distances = similarity[movie_index]
+
+    movie_list = sorted(
+        enumerate(distances),
+        key=lambda x: x[1],
+        reverse=True
+    )[1:n+1]
+
+    recommended_movies = []
+    
+
+    for i in movie_list:
         movie_id = movies.iloc[i[0]].movie_id
-        recommended_movie_names.append(movies.iloc[i[0]].title)
+        recommended_movies.append(movies.iloc[i[0]].title)
+        
 
-    return recommended_movie_names
+    return recommended_movies
 
-
-st.header('Movie Recommender System')
-movies = pickle.load(open('movies.pkl','rb'))
-similarity = pickle.load(open('similarity.pkl','rb'))
-
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
 
 if st.button('Show Recommendation'):
-    recommended_movie_names= recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
+    movie_name= recommend(selected_movie)           
+    col1, col2, col3, col4, col5 = st.columns(5)    
     with col1:
-        st.text(recommended_movie_names[0])
+        st.text(movie_name[0])
+        
 
     with col2:
-        st.text(recommended_movie_names[1])
+        st.text(movie_name[1])
+        
 
     with col3:
-        st.text(recommended_movie_names[2])
+        st.text(movie_name[2])
+        
     
     with col4:
-        st.text(recommended_movie_names[3])
+        st.text(movie_name[3])
+        
         
     with col5:
-        st.text(recommended_movie_names[4])
+        st.text(movie_name[4])
         
-
-
-
-
