b6cb193c53b9e81fe4313a4051f7c33364ceb9e5
Asmit Prem Kumar <asmitpremkumar@gmail.com>
2026-06-13 21:46:00 +0530

    Integrate movie poster fetching in recommendations
    Added functionality to fetch and display movie posters alongside recommendations.

--- app.py
+++ app.py
@@ -2,6 +2,16 @@ import streamlit as st
 import pickle
 import gdown
 import os
+import requests
+
+
+def fetch_posters(movie_id):
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=1b7c9bd68af2e3bb30fb791a10ff85c0&language=en-US".format(movie_id)
+    data = requests.get(url)
+    data = data.json()
+    poster_path = data['poster_path']
+    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
+    return full_path
 
 
 movies= pickle.load(open('movies.pkl','rb'))
@@ -39,35 +49,35 @@ def recommend(selected_movie, n=5):
     )[1:n+1]
 
     recommended_movies = []
-    
+    recommended_posters = []
 
     for i in movie_list:
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movies.append(movies.iloc[i[0]].title)
-        
+        recommended_posters.append(fetch_posters(movie_id))
 
-    return recommended_movies
+    return recommended_movies, recommended_posters
 
 
 if st.button('Show Recommendation'):
-    movie_name= recommend(selected_movie)           
+    movie_name, movie_poster= recommend(selected_movie) 
     col1, col2, col3, col4, col5 = st.columns(5)    
     with col1:
         st.text(movie_name[0])
-        
+        st.image(movie_poster[0])
 
     with col2:
         st.text(movie_name[1])
-        
+        st.image(movie_poster[1])
 
     with col3:
         st.text(movie_name[2])
-        
+        st.image(movie_poster[2])
     
     with col4:
         st.text(movie_name[3])
-        
+        st.image(movie_poster[3])
         
     with col5:
         st.text(movie_name[4])
-        
+        st.image(movie_poster[4])     
