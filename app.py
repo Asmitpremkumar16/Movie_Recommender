e36cf5837c3e0720f9e2f0a1b19dd89cdcf7444d
Asmit Prem Kumar <asmitpremkumar@gmail.com>
2026-06-14 16:33:03 +0530

    Refactor movie recommendation logic and caching

--- app.py
+++ app.py
@@ -4,60 +4,49 @@ import gdown
 import os
 import requests
 
-
+@st.cache_resource
 def fetch_posters(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=1b7c9bd68af2e3bb30fb791a10ff85c0&language=en-US".format(movie_id)
+    url = "https://api.themoviedb.org/3/movie/{}?api_key=320261176e294c7a530d6725cbeae04b&language=en-US".format(movie_id)
     data = requests.get(url)
     data = data.json()
-    poster_path = data['file_path']
+    poster_path = data['poster_path']
     full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
     return full_path
 
-
+@st.cache_resource
 movies= pickle.load(open('movies.pkl','rb'))
 file_id = "1-EHGBW_MGdvZFpQGsIcNvq4dXRVbcwxG"
 file_path = "similarity.pkl"
-
-# Download only if not already present
 if not os.path.exists(file_path):
     url = f"https://drive.google.com/uc?id={file_id}"
     gdown.download(url, file_path, quiet=False)
-
+    
+@st.cache_resource
 # Load the pickle file
 similarity = pickle.load(open(file_path, "rb"))
 movies_list= movies['title'].values
 
+def recommend(movie):
+    if movie not in movies_list:
+        print("Movie not found")
+        return
+        
+    index = movies[movies['title'] == movie].index[0]
+    distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
+    recommended_movie_names = []
+    recommended_movie_posters = []
+    for i in distances[1:6]:
+        # fetch the movie poster
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movie_posters.append(fetch_poster(movie_id))
+        recommended_movie_names.append(movies.iloc[i[0]].title)
+
+    return recommended_movie_names, recommended_movie_posters    
 
 st.set_page_config(page_title="Movie Recommender", layout="wide")
 st.header("Movie Recommender System")
 
-selected_movie= st.selectbox('Select a movie',movies_list)
-
-
-def recommend(selected_movie, n=5):
-    if selected_movie not in movies['title'].values:
-        print("Movie not found")
-        return
-
-    movie_index = movies[movies['title'] == selected_movie].index[0]
-    distances = similarity[movie_index]
-
-    movie_list = sorted(
-        enumerate(distances),
-        key=lambda x: x[1],
-        reverse=True
-    )[1:n+1]
-
-    recommended_movies = []
-    recommended_posters = []
-
-    for i in movie_list:
-        movie_id = movies.iloc[i[0]].movie_id
-        recommended_movies.append(movies.iloc[i[0]].title)
-        recommended_posters.append(fetch_posters(movie_id))
-
-    return recommended_movies, recommended_posters
-
+selected_movie = st.selectbox("Type or select a movie from the dropdown", movie_list)
 
 if st.button('Show Recommendation'):
     movie_name, movie_poster= recommend(selected_movie) 
@@ -65,19 +54,15 @@ if st.button('Show Recommendation'):
     with col1:
         st.text(movie_name[0])
         st.image(movie_poster[0])
-
     with col2:
         st.text(movie_name[1])
         st.image(movie_poster[1])
-
     with col3:
         st.text(movie_name[2])
-        st.image(movie_poster[2])
-    
+        st.image(movie_poster[2])  
     with col4:
         st.text(movie_name[3])
-        st.image(movie_poster[3])
-        
+        st.image(movie_poster[3])     
     with col5:
         st.text(movie_name[4])
         st.image(movie_poster[4])     
